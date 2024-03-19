57ceba5368720dad1025fab21a24ba51f7439dc1
Лев Чернин <cherninleva@gmail.com>
2024-03-19 17:49:14 +0300

    feat: desktop version of the price block

---
+++ source/js/tabs.js
@@ -0,0 +1,28 @@
+const buttons = document.querySelectorAll('[data-tab');
+const lists = document.querySelectorAll('[data-list]');
+
+(function () {
+
+  lists.forEach((list, i) => {
+    if (i !== 0) {
+      list.style.display = 'none';
+    }
+  });
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+
+      buttons.forEach((item) => item.classList.remove('is-active'));
+
+      lists.forEach((list) => {
+        list.style.display = 'none';
+
+        if (button.dataset.tab === list.dataset.list) {
+          button.classList.add('is-active');
+          list.style.display = 'block';
+        }
+      });
+    });
+  });
+})();
+
