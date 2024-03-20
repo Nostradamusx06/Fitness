feb9fe134444a303928b5db043a1d7d48cc5eb88
Лев Чернин <cherninleva@gmail.com>
2024-03-20 12:22:52 +0300

    fix: iterate not by list, but by item

--- source/js/tabs.js
+++ source/js/tabs.js
@@ -1,21 +1,18 @@
 (function () {
   const buttons = document.querySelectorAll('[data-index]');
-  const listsSubscriptions = document.querySelectorAll('[data-list]');
+  const subscriptionItems = document.querySelectorAll('.subscription__item');
 
   buttons.forEach((button) => {
     button.addEventListener('click', () => {
       buttons.forEach((item) => item.classList.remove('is-active'));
       button.classList.add('is-active');
 
-      listsSubscriptions.forEach((list) => {
-        const priceData = button.dataset.prices.split(',');
-        const subscriptionItems = list.querySelectorAll('.subscription__item');
+      const priceData = button.dataset.prices.split(',');
 
-        subscriptionItems.forEach((item, index) => {
-          const price = priceData[index];
-          item.querySelector('.subscription__min-text').textContent = price;
-          item.querySelector('.subscription__max-text').textContent = price;
-        });
+      subscriptionItems.forEach((item, index) => {
+        const price = priceData[index];
+        item.querySelector('.subscription__min-text').textContent = price;
+        item.querySelector('.subscription__max-text').textContent = price;
       });
     });
   });
