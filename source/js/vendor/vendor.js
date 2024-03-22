56f032535c4de5ef85cfabf924939bb69d03c7df
Лев Чернин <cherninleva@gmail.com>
2024-03-22 22:27:47 +0300

    feat: desktop, tablet and mobile versions of the jury block

---
+++ source/js/vendor/vendor.js
@@ -0,0 +1,36 @@
+import './swiper.js';
+
+const coachSwiper = document.querySelector('.coach__slider');
+
+(function () {
+  if (coachSwiper) {
+    const swiper = new Swiper(coachSwiper, {
+
+      navigation: {
+        nextEl: '.coach__button--next',
+        prevEl: '.coach__button--prev',
+      },
+      simulateTouch: false,
+      grabCursor: false,
+      watchOverflow: true,
+      loop: true,
+      speed: 600,
+      breakpoints: {
+        320: {
+          initialSlide: 2,
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1366: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+          slidesPerGroup: 1,
+        },
+      },
+    });
+  }
+})();
