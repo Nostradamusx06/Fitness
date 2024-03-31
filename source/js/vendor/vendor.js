c23fc67d1467c1953f34d42f65812deac1cdc257
Лев Чернин <cherninleva@gmail.com>
2024-03-31 15:11:10 +0300

    feat: desktop, tablet and mobile versions of the reviews block

--- source/js/vendor/vendor.js
+++ source/js/vendor/vendor.js
@@ -2,36 +2,60 @@ import './swiper.js';
 
 
 (function () {
-  const coachSwiper = document.querySelector('.coach__slider');
-  if (!coachSwiper) {
-    return;
+  const createSwiperCoach = () => {
+    const coachSwiper = document.querySelector('.coach__slider');
+    if (!coachSwiper) {
+      return;
+    }
+
+    const swiperForCoach = new Swiper(coachSwiper, {
+      navigation: {
+        nextEl: '.coach__button--next',
+        prevEl: '.coach__button--prev',
+      },
+      simulateTouch: false,
+      grabCursor: false,
+      watchOverflow: true,
+      loop: true,
+      speed: 600,
+      breakpoints: {
+        320: {
+          initialSlide: 2,
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 40,
+        },
+        1366: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+          slidesPerGroup: 1,
+        },
+      },
+    });
+  };
+
+  const createSwiperReviews = () => {
+    const reviewsSwiper = document.querySelector('.reviews__slider');
+    if (!reviewsSwiper) {
+      return;
+    }
+
+    const swiperForReviews = new Swiper(reviewsSwiper, {
+
+      navigation: {
+        nextEl: '.reviews__button--next',
+        prevEl: '.reviews__button--prev',
+      },
+      spaceBetween: 60,
+      slidesPerView: 1,
+      grabCursor: true,
+    });
   }
 
-  const swiper = new Swiper(coachSwiper, {
-    navigation: {
-      nextEl: '.coach__button--next',
-      prevEl: '.coach__button--prev',
-    },
-    simulateTouch: false,
-    grabCursor: false,
-    watchOverflow: true,
-    loop: true,
-    speed: 600,
-    breakpoints: {
-      320: {
-        initialSlide: 2,
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 40,
-      },
-      1366: {
-        slidesPerView: 4,
-        spaceBetween: 40,
-        slidesPerGroup: 1,
-      },
-    },
-  });
+  createSwiperCoach();
+  createSwiperReviews();
 })();
+
